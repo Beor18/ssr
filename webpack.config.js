a8f8f439746407f2aee31d40e5b6118427c22b4f
Fernando Lopez <lopezfer@globalhitss.com>
2020-10-03 23:43:23 -0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const client = {
   entry: './src/index.js',
   mode: process.env.NODE_ENV || 'development',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -17,4 +21,35 @@ module.exports = {
       },
     ],
   },
+}
+
+const server = {
+  entry: './server/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve('babel-loader')
+        }
+      },
+      {
+        test: /\.graphql$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'raw-loader'
+        }
+      }
+    ]
+  },
 };
+
+module.exports = [client, server];
