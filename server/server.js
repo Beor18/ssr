7324cf0e1d0c223639eb2a4f648eea9654b33ab6
Fernando Lopez <lopezfer@globalhitss.com>
2020-10-08 15:01:36 -0300

    change style

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ function renderFullPage(html) {
       <head>
         <title>Server side rendering - Platzi</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+        <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+        
         <meta name="description" content="Examén de ssr para Platzi con react y express"/>
         <meta name="keywords" content="ssr, platzi, ssr con react, express"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
